8993b7a8d3e267986fcaaf4ee20cfaf38e91c892
travisbrowning <travisbrowning@me.com>
2023-05-26 20:40:29 -0500

    Initial commit, feature complete

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#new-item-form")
+const list = document.querySelector("#list")
+const input = document.querySelector("#item-input")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const item = document.createElement('div')
+    item.innerText = input.value
+    item.classList.add('list-item')
+
+
+    list.appendChild(item)
+
+    input.value = " "
+
+    item.addEventListener('click', () => {
+        item.remove()
+    })
+})
